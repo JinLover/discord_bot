d3151e54841fb01290cbbcbf4d0a3f1122f2e221
JinLover <51523331+JinLover@users.noreply.github.com>
2020-12-23 16:16:24 +0900

    .

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import discord
 import asyncio
-#import requests
+import requests
 import datetime
 
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
